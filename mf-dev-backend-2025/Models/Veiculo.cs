99097a8b9c7d64ac787b3487a005a78bcd9c511b
Yago <yagolobo.dev@gmail.com>
2025-04-14 17:29:56 -0300

    feat:adicionando modelo veiculo

---
+++ mf-dev-backend-2025/Models/Veiculo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2025.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Obrigatorio informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o placa!")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o Ano de Fabricacao!")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o Ano do Modelo!")]
+        public int AnoModelo { get; set; }
+
+    }
+}
